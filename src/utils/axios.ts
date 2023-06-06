5268769ae1550b37dbbdfbd130801a97affda54d
inTheMembrane <naughtnaught@outlook.com>
2023-06-06 15:11:41 +0200

    CHORE try and fix again

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 const getToken = async () => {
   const tokenWithoutQuotes = await localStorage.getItem('token')?.replace(/^"(.*)"$/, '$1');
+  console.log('tokenWithoutQuotes : ',tokenWithoutQuotes)
   return tokenWithoutQuotes;
+
 };
 // eslint-disable-next-line import/prefer-default-export
 export const axiosInstance = axios.create({
