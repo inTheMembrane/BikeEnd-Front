147c7e346bcc28e96e07e430275fca5601fa08ed
inTheMembrane <naughtnaught@outlook.com>
2023-06-09 18:33:34 +0200

    CHORE format and await token

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
 const getToken = async () => {
-  const tokenWithoutQuotes = await localStorage.getItem('token')?.replace(/^"(.*)"$/, '$1');
-  console.log('tokenWithoutQuotes : ',tokenWithoutQuotes)
-  return tokenWithoutQuotes;
-
+  const token = await localStorage.getItem('token');
+  let formattedToken = '';
+  token ? formattedToken = token.replace(/^"(.*)"$/, '$1') : formattedToken = '';
+  // const tokenWithoutQuotes = await localStorage.getItem('token')?.replace(/^"(.*)"$/, '$1');
+  // console.log('tokenWithoutQuotes : ', tokenWithoutQuotes);
+  console.log('formattedToken : ', formattedToken);
+  return formattedToken;
 };
 // eslint-disable-next-line import/prefer-default-export
 export const axiosInstance = axios.create({
